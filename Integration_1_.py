df14589b29035a2dab231b217380edacf65d5a3d
Tharakadhanushka <55351120+Tharakadhanushka@users.noreply.github.com>
2020-02-13 22:05:54 +0530

    Add files via upload

---
+++ Integration_1_.py
@@ -0,0 +1,54 @@
+# Finding the area under y = f(x) = 3x/5 + 2 between the limits [a,b]
+
+import numpy as np
+from random import random
+import matplotlib.pyplot as plt
+
+def func(xval):
+    yval = (3/5)*xval + 2
+    return yval
+    
+
+def max_yval(a,b):
+    temp_x = np.arange(a,b,0.1)
+    temp_y = func(temp_x)          #(3/5)*temp_x +2      # y = f(x) = 3x/5 + 2
+    max_y =  temp_y.max()
+    return max_y
+
+
+a = 0
+b = 5
+max_Px = max_yval(a,b) 
+
+
+N = 0
+M = 0
+num =100000
+x_array = np.zeros(num)
+y_array = np.zeros(num)
+
+x_in = []
+y_in = []
+
+for i in range(0,num):
+    x =  a + (b-a)*random()  #x value between [a,b]
+    y =  0 + (max_Px-0)*random()  #y value between [0,max_Px]
+    yval = func(x)
+    if( y <= yval ):
+        M = M + 1
+        x_in.append(x)
+        y_in.append(y)
+        
+    N = N + 1
+    x_array[i] = x
+    y_array[i] = y
+
+Area = max_Px*(b-a)*M/N
+print("Area = ", Area)    
+plt.plot(x_array, y_array, '.', color='blue')
+plt.plot(x_in, y_in, '.', color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.gca().set_title('y vs x')
+plt.show()
+
---
+++ Integration_3_.py
@@ -0,0 +1,51 @@
+# Finding the area under y = 0.5x+0.5 line and y= x^2 curve between the limits [a,b]
+
+import numpy as np
+from random import random
+
+def curve(xval):
+    yval = xval**2
+    return yval
+
+def line(xval):
+    yval = 0.5*xval+0.5
+    return yval
+
+
+def Area(a,b,function):
+    def func(xval):
+        yval = function(xval)
+        return yval
+       
+    def max_yval(a,b):
+        temp_x = np.arange(a,b+0.1,0.1)   #(start value,end,increment) end is not included.need to have a vlue just above b
+        temp_y = func(temp_x)          #temp_x**2      # y = f(x) = x**2
+        max_y =  temp_y.max()
+        return max_y
+    
+    max_Px = max_yval(a,b) 
+    N = 0
+    M = 0
+    num =100000
+    for i in range(0,num):
+        x =  a + (b-a)*random()  #x value between [a,b]
+        y =  0 + (max_Px-0)*random()  #y value between [0,max_Px]
+        yval = func(x)
+        if( y <= yval ):
+            M = M + 1       
+        N = N + 1
+
+
+    Area = max_Px*(b-a)*M/N
+    print("Area = ", Area)
+    return Area
+
+a = -0.5   # lower limit
+b = 1      # upper limit
+Area_under_curve = Area(a,b,curve)
+Area_under_line  = Area(a,b,line)
+
+Area_shaded = Area_under_line - Area_under_curve
+print("Area Shaded = ",Area_shaded)
+
+
